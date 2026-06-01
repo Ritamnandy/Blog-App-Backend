8d2f20618b3de0544ce84dcaf0cc1f0616bd2c75
Ritamnandy <nandyritam519@gmail.com>
2026-06-01 19:39:28 +0000

    add cloudinary upload utility for image handling

---
+++ server/src/utils/cloudinary.upload.js
@@ -0,0 +1,36 @@
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+
+
+cloudinary.config( {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+} )
+
+const uploadCloudinary = async ( filePath ) =>
+{
+    try
+    {
+        if ( !filePath )
+        {
+            console.log( "file not provide" );
+            return null
+
+        };
+        const result = await cloudinary.uploader.upload( filePath, {
+            resource_type: "auto"
+        } )
+        fs.unlinkSync( filePath )
+        return result;
+    } catch ( error )
+    {
+        fs.unlinkSync( filePath )
+        console.log( "cloudinary upload error:- ", error.message );
+        return null;
+    }
+}
+
+export { uploadCloudinary }
